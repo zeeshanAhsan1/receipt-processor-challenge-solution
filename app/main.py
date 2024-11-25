1a1e4e15425bbb504768b9c06707f6c25d687aeb
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-11-25 18:51:44 +0530

    Add points logic and mod validations.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel, Field
 from typing import List, Dict
 import uuid
+from math import ceil
+from datetime import datetime
 
 # Init App
 app = FastAPI(
@@ -53,12 +55,17 @@ class ReceiptResponse(BaseModel):
           )
 def processReceipt(receipt: Receipt):
 
-  # Basic validation -> If the retailer name doesn't have alpha-numeric characters -> throw Ex -> Send 400
+  # Basic validation -> If the no (retailer name or purchase date or purchase time) or no items -> throw Ex -> Send 400
   try:
-    retailer_name = receipt.retailer
-    for char in retailer_name:
-      if not char.isalnum():
-        raise ValueError("Invalid retailer name")
+    #for char in retailer_name:
+    if not receipt.retailer:
+      raise ValueError("No retailer name")
+    if not receipt.purchaseDate:
+      raise ValueError("No purchase date")
+    if not receipt.purchaseTime:
+      raise ValueError("No purchase time")
+    if len(receipt.items)==0:
+      raise ValueError("No items in request")
   except ValueError as e:
     raise HTTPException(
       status_code=status.HTTP_400_BAD_REQUEST,
@@ -71,7 +78,7 @@ def processReceipt(receipt: Receipt):
   # Store generated receipt in memory
   receipt_store[receipt_id] = receipt.dict()
 
-  print(receipt_store)
+  #print(receipt_store)
 
   # Return response
   return {"id": receipt_id}
@@ -81,8 +88,24 @@ def processReceipt(receipt: Receipt):
   summary="Get points for a receipt",
   description="Looks up the receipt by ID and returns the number of points awarded.",
   status_code=status.HTTP_200_OK,
+  responses={
+        404: {
+            "description": "Receipt not found",
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "type": "object",
+                        "properties": {
+                            "detail": {"type": "string", "example": "Receipt not found"}
+                        },
+                    }
+                }
+            },
+        }
+    },
   )
 def getPoints(id: str):
+  # If receipt is not present in receipt store -> throw error and send 404 status code
   try:
     receipt = receipt_store.get(id)
     if not receipt:
@@ -92,5 +115,43 @@ def getPoints(id: str):
       status_code=status.HTTP_404_NOT_FOUND,
       detail="Receipt not found with Id: " + id,
     )
-  points = 0
+  points = calculatePoints(receipt)
+
   return {"points": points}
+
+def calculatePoints(receipt):
+  points = 0
+  # 1. One point for every alphanumeric character in the retailer name.
+  points += sum(1 for char in receipt["retailer"] if char.isalnum())
+
+
+  # 2. 50 points if the total is a round dollar amount with no cents.
+  total = float(receipt["total"])
+  if total.is_integer():
+    points += 50
+
+  # 3. 25 points if the total is a multiple of 0.25.
+  if total % 0.25 == 0:
+    points += 25
+
+  # 4. 5 points for every two items on the receipt.
+  points += (len(receipt["items"]) // 2) * 5
+
+  # 5. Points based on the item description length.
+  for item in receipt["items"]:
+    trimmed_length = len(item["shortDescription"].strip())
+    if trimmed_length % 3 == 0:
+      price_points = ceil(float(item["price"]) * 0.2)
+      points += price_points
+
+  # 6. 6 points if the day in the purchase date is odd.
+  purchase_date = datetime.strptime(receipt["purchaseDate"], "%Y-%m-%d")
+  if purchase_date.day % 2 == 1:
+    points += 6
+
+  # 7. 10 points if the purchase time is between 2:00pm and 4:00pm.
+  purchase_time = datetime.strptime(receipt["purchaseTime"], "%H:%M").time()
+  if purchase_time >= datetime.strptime("14:00", "%H:%M").time() and purchase_time < datetime.strptime("16:00", "%H:%M").time():
+    points += 10
+
+  return points
